27a327027cedb390293af4d984792ba2d70fd025
Warren Shipp <s3690682@student.rmit.edu.au>
2018-08-12 20:07:28 +1000

    started bst

--- AlgorithmsAss1/javaSrc/javaSrc/BstMultiset.java
+++ AlgorithmsAss1/javaSrc/javaSrc/BstMultiset.java
@@ -3,16 +3,64 @@ import java.util.*;
 
 public class BstMultiset<T> extends Multiset<T>
 {
-	public BstMultiset() {
+	
+	private Node root;
+	
+	public class Node
+	{
+		String key;
+		Node leftChild;
+		Node rightChild;
+		int count;
+		
+		public Node(T item)
+		{
+			this.key = item.toString();
+			this.leftChild = null;
+			this.rightChild = null;
+			this.count = 0;
+		}
+	}
+	public BstMultiset() 
+	{
+		this.root = null;
+		
 		// Implement me!
 	} // end of BstMultiset()
 
-	public void add(T item) {
+	public void add(T item) 
+	{
+		root = addAgain(root, item);
 		// Implement me!
 	} // end of add()
 
 
-	public int search(T item) {
+	public Node addAgain(Node current, T item)
+	{
+		String itemStr = item.toString();
+		if(root == null)
+		{
+			root = new Node(item);
+			root.count++;
+			return root;
+		}
+		if(itemStr.compareTo(root.key) < 0 )
+		{
+			root.leftChild = addAgain(root.leftChild, item);
+		}
+		else if(itemStr.compareTo(root.key) > 0)
+		{
+			root.rightChild = addAgain(root.rightChild, item);
+		}
+		else
+		{
+			root.count++;
+			return root;
+		}
+		return root;
+	}
+	public int search(T item) 
+	{
 		// Implement me!
 
 		// default return, please override when you implement this method
@@ -20,17 +68,20 @@ public class BstMultiset<T> extends Multiset<T>
 	} // end of add()
 
 
-	public void removeOne(T item) {
+	public void removeOne(T item)
+	{
 		// Implement me!
 	} // end of removeOne()
 	
 	
-	public void removeAll(T item) {
+	public void removeAll(T item)
+	{
 		// Implement me!
 	} // end of removeAll()
 
 
-	public void print(PrintStream out) {
+	public void print(PrintStream out)
+	{
 		// Implement me!
 	} // end of print()
 
